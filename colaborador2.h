f78178ae56962ffb74f866c72d3708c9746dbb29
Shaden Bravo Mendoza <86112520+Shadenbm@users.noreply.github.com>
2021-07-10 00:24:45 -0500

    Update colaborador2.h
    El producto de matrices de (5x5) lo realicé definiendo las variables i para filas y j para columnas, utilizando el ciclo FOR para contar y acumular las variables. En la línea 22 muestro la matriz1 y en la 31 la matriz2, por ultimo se muestra el resultado de la multiplicación en la línea 40.

--- colaborador2.h
+++ colaborador2.h
@@ -1,9 +1,40 @@
-void producto(float (*m1)[5], float (*m2)[5], float (*r)[5])
+void producto(float (*m1)[5], float (*m2)[5],float (*r)[5])
 {
+	int filas=5,columnas=5;
+	cout<<"||-----PRODUCTO DE LAS MATRICES-----||\n";
 
-cout<<"Obetener el producto de las dos matrices ";
-
-
-
-
+	for(int i=0;i<filas;i++)
+	{
+		for(int j=0;j<columnas;j++)
+			{
+				r[i][j]+=m1[i][j]*m2[i][j];
+			} 
+	}
+	cout<<"\nMostrando matriz #1\n\n";
+	for(int i=0;i<filas;i++)
+	{
+		for(int j=0;j<columnas;j++)
+		{
+			cout<<m1[j][i]<<" ";
+		}
+		cout<<"\n\n";
+	}	
+	cout<<"\nMostrando matriz #2\n\n";
+	for(int i=0;i<filas;i++)
+	{
+		for(int j=0;j<columnas;j++)
+		{
+			cout<<m2[j][i]<<"  ";
+		}
+		cout<<"\n\n";
+	}
+	cout<<"El producto de las dos matrices es: \n\n";
+	for(int i=0;i<filas;i++)
+	{
+		for(int j=0;j<columnas;j++)
+		{
+			cout<<r[j][i]<<" ";
+		}
+		cout<<"\n\n";
+	}
 }
