a4c25e1ee7671e4d174eef3f266b5c05d1e33c8d
Fernando López <86112189+fernando-2906@users.noreply.github.com>
2021-07-10 00:59:11 -0500

    Update colaborador5.h
    En este código realice la declaración de variables : "Nf" para numero de filas ,"Nc" `para el numero de columnas ,"r" para la primera respuesta, "r2" en donde se guardara la segunda respuesta "med" y "med2" para la media de la primera y segunda matriz,, "var" y "var2" para la varianza de la matriz 1 y 2.
    En la línea 13 y 38 se hace la operación  de la media para las matrices y en las líneas 25 y 50 la operación para la varianza, además utilice el ciclo for para recorrer toda la matriz

--- colaborador5.h
+++ colaborador5.h
@@ -1,9 +1,56 @@
-void mediayvarianza(float (*m1)[5], float (*m2)[])
+void mediayvarianza(float (*m1)[5], float(*m2)[5])
+
 {
+	cout<<"||-----MEDIA Y VARIANZA-----||\n";
+	int Nf=5,Nc=5, r=0,r2=0;
+	int  med=0,med2=0,var=0,var2=0;
 
-cout<<"Determinar  cual de las 2 matrices tiene la mayor media y varianza";
-
-
-
-
+	cout<<"\nMedia matriz #1\n\n";
+	for (int h=0; h<Nf;h++)
+	{
+      	for (int c=0;c<Nc;c++)
+      {
+      		r+=m1[h][c];
+    	}
+ 	}
+  med= r/25;
+	cout<<med;
+	cout<<"\n";
+	
+	cout<<"\nVarianza matriz #1\n\n";
+	for (int h=0; h<Nf;h++)
+	{
+      	for (int c=0;c<Nc;c++)
+		    {
+      		  var=pow (m1[h][c]*med,25);
+   		  }
+    
+  }
+	var/=25;
+	cout<<var;
+	cout<<"\n";
+	
+	cout<<"\nMedia matriz #2\n\n";
+	for (int h=0; h<Nf;h++)
+	{
+      	for (int c=0;c<Nc;c++)
+		    {
+      		  r2+=m1[h][c];
+    	  }
+  }
+  med2= r2/25;
+	cout<<med2;
+	cout<<"\n";
+	
+	cout<<"\nVarianza matriz #2\n\n";
+	for (int h=0; h<Nf;h++)
+	{
+      for (int c=0;c<Nc;c++)
+		  {
+      	  var2=pow (m1[h][c]*med2,25);
+    	}
+ 	}
+	var2/=25;
+	cout<<var2;
+	cout<<"\n";
 }
