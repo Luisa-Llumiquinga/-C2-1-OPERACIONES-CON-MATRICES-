e2292fc635bd19ee36680f9b8a9696c5434a3bec
Luisa-Llumiquinga <86111634+Luisa-Llumiquinga@users.noreply.github.com>
2021-07-10 00:01:42 -0500

    Update colaborador4.h
    En este archivo he realizado la transpuesta de una matriz (5x5), donde utilicé el ciclo FOR para contar y acumular las variables definidas I y J que llegan a ser las filas y columnas, en la línea 20 es donde se muestra el resultado final.

--- colaborador4.h
+++ colaborador4.h
@@ -1,9 +1,24 @@
-void transpuesta(float (*m1)[5], float (*r)[5])
+void transpuesta(float (*m1)[5], float(*r)[5])
 {
+	cout<<"||-----TRANSPUESTA-----||\n";
 
-cout<<"Obtener la transpuesta de una matriz";
-
-
-
-
+  cout<<"Matriz Normal\n";
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+			cout<<m1[i][j]<<" ";
+		}
+		cout<<"\n";
+	}	
+  
+	cout<<"\nMatriz Transpuesta\n";
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+			cout<<m1[j][i]<<"  ";
+		}
+		cout<<"\n";
+	}	
 }
